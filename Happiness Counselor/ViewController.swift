d5f478bbeea6b270cf120c4805e359985df7e001
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2015-08-15 23:45:59 -0700

    Initial Commit

---
+++ Happiness Counselor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Happiness Counselor
+//
+//  Created by Matthew Allen Lin on 8/15/15.
+//  Copyright (c) 2015 Matthew Allen Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
